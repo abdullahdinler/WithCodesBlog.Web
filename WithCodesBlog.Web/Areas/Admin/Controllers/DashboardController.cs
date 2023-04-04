623d910be35692fe50b8a92553146eea7d2f8d43
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2023-04-05 00:25:24 +0300

    Admin Paneli Yapılan İşlemler: -> Gösterge paneli tasarımı yapıldı.

--- WithCodesBlog.Web/Areas/Admin/Controllers/DashboardController.cs
+++ WithCodesBlog.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WithCodesBlog.Web.UI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin,Yazar")]
     public class DashboardController : Controller
     {
         public IActionResult Index()
